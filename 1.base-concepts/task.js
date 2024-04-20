3bb36a5e6ad56afc2c57c58ffef7e91f72ebef8d
test1103 <mikimiki89@yandex.ru>
2024-04-20 13:40:21 +0300

    task basicСoncepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2)-4*a*c;
+  if (d === 0) {
+    arr.push(-1*b/(2*a));
+    return arr;
+  } 
+  if(d > 0){
+    arr.push((-b + Math.sqrt(d))/(2*a));
+    arr.push((-b - Math.sqrt(d))/(2*a));
+    return arr;
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percen = percent/100;
+  let oneMounthPercent = percen/12;
+  let loanAmount = amount-contribution
+  let payment = loanAmount * (oneMounthPercent + (oneMounthPercent / (((1 + oneMounthPercent)**countMonths) - 1)));
+
+  return Math.round(payment*countMonths*100)/100;
 }
