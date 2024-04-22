e77e9f72541b561ba56250395a20d0afcc110a62
test1103 <mikimiki89@yandex.ru>
2024-04-23 01:01:18 +0300

    task tryCatch v1.0

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,24 @@
-﻿
+﻿function parseCount(a){
+    let num = Number.parseFloat(a);
+    if(Number.isNaN(num)){
+       throw new Error('Невалидное значение');
+    }
+    return num;
+}
+
+function validateCount(item){
+    try{
+        return parseCount(item)
+    } catch(error){
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a,b,c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+}
