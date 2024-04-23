5a331c4db639dbbc0646c9541d3dab134169defc
test1103 <mikimiki89@yandex.ru>
2024-04-23 21:26:18 +0300

    task throw v1.1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -8,7 +8,7 @@
 
 function validateCount(item){
     try{
-        return parseCount(item)
+        return parseCount(item);
     } catch(error){
         return error;
     }
@@ -16,27 +16,27 @@ function validateCount(item){
 
 class Triangle {
     constructor(a, b, c){
-        if(a+b < c || b+c <a || c+a <b){
-            throw new Error('Треугольник с такими сторонами не существует')
+        if(a + b < c || b + c <a || c + a <b){
+            throw new Error('Треугольник с такими сторонами не существует');
         } 
         this.a = a;
         this.b = b;
         this.c = c;
     }
     get perimeter(){
-        let perimtr = this .a + this.b + this.c;
+        let perimtr = this.a + this.b + this.c;
         return perimtr;
     }
     get area(){
-        let p = 0.5*(this.a+this.b+this.c);
-        let area = Math.round(1000 * Math.sqrt(p * (p - this.a) * (p-this.b) * (p-this.c)))/1000;
+        let p = 0.5*(this.a + this.b + this.c);
+        let area = Math.round(1000 * Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)))/1000;
         return area; 
     }
 }
 
 function getTriangle(a, b, c){
     try{
-        let triangle = new Triangle(a,b,c);
+        let triangle = new Triangle(a, b, c);
         return triangle;
     } 
     catch(error){
