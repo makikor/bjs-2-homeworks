ee087ec61a2f29d875d6346811224b5ac6f828c6
test1103 <mikimiki89@yandex.ru>
2024-04-23 21:22:25 +0300

    task throw v1.0

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,10 +15,40 @@ function validateCount(item){
 }
 
 class Triangle {
-    constructor(a,b,c){
+    constructor(a, b, c){
+        if(a+b < c || b+c <a || c+a <b){
+            throw new Error('Треугольник с такими сторонами не существует')
+        } 
         this.a = a;
         this.b = b;
         this.c = c;
     }
-
+    get perimeter(){
+        let perimtr = this .a + this.b + this.c;
+        return perimtr;
+    }
+    get area(){
+        let p = 0.5*(this.a+this.b+this.c);
+        let area = Math.round(1000 * Math.sqrt(p * (p - this.a) * (p-this.b) * (p-this.c)))/1000;
+        return area; 
+    }
 }
+
+function getTriangle(a, b, c){
+    try{
+        let triangle = new Triangle(a,b,c);
+        return triangle;
+    } 
+    catch(error){
+        let result = {
+            get perimeter(){
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area(){
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+        return result;
+    }
+}
+
